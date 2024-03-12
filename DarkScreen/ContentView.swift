0b5c227defa61871b1683aa59d155168c4846136
Vida-CruX <esrkforward@gmail.com>
2024-03-12 17:14:31 +0800

    Initial Commit

---
+++ DarkScreen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DarkScreen
+//
+//  Created by 陈鹤 on 12/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DarkScreen/DarkScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkScreenApp.swift
+//  DarkScreen
+//
+//  Created by 陈鹤 on 12/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct DarkScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
